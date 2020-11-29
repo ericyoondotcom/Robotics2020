5e40bcd2f9b3a4deefad7672a14ca4f51acc6649
Eric <emjyoon@gmail.com>
2020-11-28 16:13:20 -0800

    Add kenny test drive

---
+++ kennyTestdrive/src/robot-config.cpp
@@ -0,0 +1,27 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor intakeR = motor(PORT14, ratio18_1, true);
+motor intakeL = motor(PORT11, ratio18_1, false);
+motor backRollers = motor(PORT15, ratio18_1, true);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
